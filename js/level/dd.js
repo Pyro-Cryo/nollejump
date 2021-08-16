7b98776654e2b4b5792071b9dea42c5a0c0dde65
jonasnylund <35763296+jonasnylund@users.noreply.github.com>
2021-08-16 23:35:21 +0200

    Gruprog, lika svår som i verkligheten

--- js/level/dd.js
+++ js/level/dd.js
@@ -82,8 +82,152 @@ Level.levels.set("DD1331", (infoOnly) => {
 	if (infoOnly)
 		return level;
 
+	let width = controller.gameArea.gridWidth;
+	let spacing = 250;
+
+	const variables = level.defineRegion("intro to variables");
+	variables.wait(spacing).spawn(Platform, 3, (e, history, level) => [
+		width/4,
+		level.yCurrent
+		]).spaced(spacing);
+
+	const integers = level.defineRegion("integers");
+
+	integers.wait(spacing)
+		.spawn(DiscreteMovingPlatform, 5, (e, history, level) => [
+		width*3/4, level.yCurrent]).spaced(spacing);
+	integers.wait(spacing*2/3).spawn(Homework, 1, (e,h,l) => [width*3/5, l.yCurrent]).immediately();
+	integers.wait(spacing/3).spawn(Platform, 1, (e,h,l) => [width/3, l.yCurrent]).immediately();
+	integers.wait(spacing/2);
+
+	const datatypes = level.defineRegion("datatypes");
+	datatypes.spawn(BasicMovingPlatform, 10, (e,h,l) => [
+		width/5 + width/3 * Math.random(),
+		l.yCurrent
+		]).spaced(spacing * 2);
+	datatypes.interleave(new Region()
+		.wait(spacing)
+		.spawn(Platform, 5, (e,h,l) => 
+			[width/3, l.yCurrent]).spaced(spacing * 2)
+		.wait(spacing*2)
+		.spawn(Platform, 5, (e,h,l) =>
+			[width*2/3, l.yCurrent]).spaced(spacing * 2));
+
+	datatypes.interleave(new Region()
+		.wait(spacing)
+		.spawn(FakePlatform, 5, (e,h,l) => 
+			[width*2/3, l.yCurrent]).spaced(spacing * 2)
+		.wait(spacing*2)
+		.spawn(FakePlatform, 5, (e,h,l) =>
+			[width/3, l.yCurrent]).spaced(spacing * 2));
+
+	datatypes.interleave(new Region()
+		.wait(datatypes.length*5/6 + spacing*3)
+		.spawn(Homework, 1, (e,h,l) => 
+			[width/3, l.yCurrent]).immediately());
+
+
+	const boost = level.defineRegion("Boost")
+	boost.spawn(Platform, 11, (e,h,l) => [
+		width/2*(1 + 1/2 * Math.sign(Math.random()-1/2)),
+		l.yCurrent
+		]).spaced(spacing)
+		.wait(spacing)
+		.spawn(Platform, 1, (e,h,l) => [
+			width/4,
+			l.yCurrent
+			]).immediately();
+	boost.interleave(new Region()
+		.wait(spacing/2)
+		.spawn(FakePlatform, 11, (e,h,l) => [
+			width/2*(1 + 1/2 * Math.sign(Math.random()-1/2)),
+			l.yCurrent
+			]).spaced(spacing)
+		.wait(spacing)
+		.spawn(FakePlatform, 1, (e,h,l) => [
+			width/4,
+			l.yCurrent
+			]).immediately()
+		);
+	boost.wait(spacing)
+		.spawn(Platform, 1, (e,h,l) => 
+			[width/4,
+			l.yCurrent]).immediately()
+		.spawn(Homework, 1, (e,h,l) => 
+			[width*3/4,
+			l.yCurrent + 40
+			]).immediately()
+		.spawn(RocketToken, 1, (e,h,l) => 
+			[width*3/4,
+			l.yCurrent - 5
+			]).immediately();
+
+	boost.spawn(FakePlatform, 1, (e,h,l) => 
+		[width*3/4,
+		l.yCurrent+10]).immediately()
+		.wait(spacing)
+		.spawn(FakePlatform, 5, (e,h,l) =>
+			[width*Math.random(),
+			l.yCurrent
+			]).spaced(spacing)
+		.wait(spacing * 24);
+
+	boost.spawn(Platform, 6, (e,h,l) => 
+		[width * Math.random(),
+		l.yCurrent
+		]).spaced(spacing/4)
+
+
+	const ks = level.defineRegion("P-uppgift");
+	ks.spawn(BasicMovingPlatform, 16, (e,h,l) =>
+		[width * Math.random(), l.yCurrent]).spaced(spacing)
+		.spawn(Platform, 5, (e,h,l) => [
+			width * Math.random(),
+			l.yCurrent
+			]).spaced(spacing);
+
+	ks.interleave(new Region()
+		.wait(spacing*2/3)
+		.spawn(Platform, 5, (e,h,l) => [
+			width * 2/3,
+			l.yCurrent
+			]).immediately()
+		.spawn(JumpBoostToken, 1, (e,h,l) => [
+			h[h.length-1].xSpawn,
+			h[h.length-1].ySpawn + 25
+			]).immediately()
+		.wait(spacing * 2)
+		.spawn(Platform, 3, (e,h,l) => [
+			width * 2/3 + width/4 * (Math.random()-1/2),
+			l.yCurrent
+			]).spaced(spacing * 2)
+		.spawn(KS, 1, (e,h,l) => [
+			h[h.length-1].xSpawn,
+			h[h.length-1].ySpawn + 25
+			]).immediately()
+		.spawn(Platform, 4, (e,h,l) => [
+			width * 2/3 + width/4 * (Math.random()-1/2),
+			l.yCurrent
+			]).spaced(spacing * 2)
+		);
+
 
 	// ...
+
+	level.initialRegion(variables);
+	variables.follower(integers);
+
+	integers.follower(datatypes, 1, level => level.homeworkCurrent < 1);
+	integers.follower(variables, 1, level => level.homeworkCurrent >= 1);
+
+	datatypes.follower(variables, 1, level => level.homeworkCurrent < 1);
+	datatypes.follower(boost, 1, level => level.homeworkCurrent >= 1);
+
+	boost.follower(integers, 1, level => level.homeworkCurrent < 2);
+	boost.follower(datatypes, 1, level => level.homeworkCurrent < 3);
+	boost.follower(ks, 1, level => level.homeworkCurrent >= 3);
+
+	ks.follower(variables);
 
 	return level;
 });
